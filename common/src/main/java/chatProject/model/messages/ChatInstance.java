f5422cb82fb9fe8a29511cc60aa9c3377f606b99
Adrien RICHARD <richardadrien0@gmail.com>
2020-10-01 12:11:36 +0200

    [fix] room creation

--- common/src/main/java/chatProject/model/messages/ChatInstance.java
+++ common/src/main/java/chatProject/model/messages/ChatInstance.java
@@ -92,7 +92,7 @@ public class ChatInstance<T> {
      * @return the new chat instance
      */
     public static <T> ChatInstance<T> initEmptyChat() {
-        return new ChatInstance<T>(new ArrayList<>(), null);
+        return new ChatInstance<T>(new ArrayList<>(), new HashMap<>());
     }
 
 }
