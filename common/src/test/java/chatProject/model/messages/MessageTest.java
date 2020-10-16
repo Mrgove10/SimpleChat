aac957a7684b634f56d92cfa574d6716bea3a4f9
Adrien RICHARD <richardadrien0@gmail.com>
2020-10-16 10:11:59 +0200

    [import Opti]

--- common/src/test/java/chatProject/model/messages/MessageTest.java
+++ common/src/test/java/chatProject/model/messages/MessageTest.java
@@ -5,8 +5,7 @@ import chatProject.model.user.UserAccount;
 import chatProject.model.user.UserInfo;
 import org.junit.Test;
 
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class MessageTest {
 
--- common/src/test/java/chatProject/model/user/UserAccountTest.java
+++ common/src/test/java/chatProject/model/user/UserAccountTest.java
@@ -2,7 +2,7 @@ package chatProject.model.user;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class UserAccountTest {
 
